715469473af14b9c6785b919344e078a847f95e3
david luis <davidluis2020@gmail.com>
2021-10-22 17:19:30 +0100

    start working on problem controller

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -9,11 +9,12 @@ class ProblemsController < ApplicationController
     @problem = Problem.new(problem_params)
     @problem.user = current_user
     authorize @problem
+    
     @problem.save ? (redirect_to problem_path(@problem)) : (render :new)
   end
 
   def show
-    
+    @problem = Problem.find(params[:id])
   end
 
   def index
